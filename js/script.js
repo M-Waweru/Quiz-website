49bc321f5845296e3f2f41d8d6d49ea986fa571e
johnngugi <johnngugi777@gmail.com>
2018-06-20 15:31:08 +0300

    Add javascript functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,47 @@
 $(document).ready(function() {
 
-	$('.button-collapse').sideNav();
+    $('.button-collapse').sideNav();
+
+    const quizContainer = $('#quiz');
+    const resultsContainer = $('#results');
+    const submitButton = $('#submitBtn');
+
+    function buildQuiz() {}
+
+    function showResults() {}
+
+    // display quiz right away
+    buildQuiz();
+
+    // on submit, show result
+    submitButton.click(showResults);
+
+    const myQuestions = [{
+        question: "Who is the strongest?",
+        answers: {
+            a: "Superman",
+            b: "The Terminator",
+            c: "Waluigi, obviously"
+        },
+        correctAnswer: "c"
+    }, {
+        question: "What is the best site ever created?",
+        answers: {
+            a: "SitePoint",
+            b: "Simple Steps Code",
+            c: "Trick question; they're both the best"
+        },
+        correctAnswer: "c"
+    }, {
+        question: "Where is Waldo really?",
+        answers: {
+            a: "Antarctica",
+            b: "Exploring the Pacific Ocean",
+            c: "Sitting in a tree",
+            d: "Minding his own business, so stop asking"
+        },
+        correctAnswer: "d"
+    }];
+
+
 });
