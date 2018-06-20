3dc4c8c6270b80f3ce2d92615a3e069c176d8f9b
johnngugi <johnngugi777@gmail.com>
2018-06-20 20:02:07 +0300

    Modify functions to use jQuery

--- js/script.js
+++ js/script.js
@@ -1,112 +1,111 @@
 $(document).ready(function() {
 
-    $('.button-collapse').sideNav();
+    (function() {
+        const quizContainer = $('#quiz');
+        const resultsContainer = $('#results');
+        const submitButton = $('#submitBtn');
 
-    const quizContainer = $('#quiz');
-    const resultsContainer = $('#results');
-    const submitButton = $('#submitBtn');
+        const myQuestions = [{
+            question: "Who is the strongest?",
+            answers: {
+                a: "Superman",
+                b: "The Terminator",
+                c: "Waluigi, obviously"
+            },
+            correctAnswer: "c"
+        }, {
+            question: "What is the best site ever created?",
+            answers: {
+                a: "SitePoint",
+                b: "Simple Steps Code",
+                c: "Trick question; they're both the best"
+            },
+            correctAnswer: "c"
+        }, {
+            question: "Where is Waldo really?",
+            answers: {
+                a: "Antarctica",
+                b: "Exploring the Pacific Ocean",
+                c: "Sitting in a tree",
+                d: "Minding his own business, so stop asking"
+            },
+            correctAnswer: "d"
+        }];
 
-    function buildQuiz() {
-        // we'll need a place to store the HTML output
-        const output = [];
+        function buildQuiz() {
+            // we'll need a place to store the HTML output
+            const output = [];
 
-        // for each question...
-        myQuestions.forEach(
-            (currentQuestion, questionNumber) => {
+            // for each question...
+            myQuestions.forEach(
+                (currentQuestion, questionNumber) => {
 
-                // we'll want to store the list of answer choices
-                const answers = [];
+                    // we'll want to store the list of answer choices
+                    const answers = [];
 
-                // and for each available answer...
-                for (letter in currentQuestion.answers) {
+                    // and for each available answer...
+                    for (letter in currentQuestion.answers) {
 
-                    // ...add an HTML radio button
-                    answers.push(
-                        `<label>
+                        // ...add an HTML radio button
+                        answers.push(
+                            `<label>
                             <input type="radio" name="question${questionNumber}" value="${letter}">
-                                ${letter} :${currentQuestion.answers[letter]}
+                                ${letter} : ${currentQuestion.answers[letter]}
                         </label>`
+                        );
+                    }
+
+                    // add this question and its answers to the output
+                    output.push(
+                        `<div class="question"> ${currentQuestion.question} </div>
+                    <div class="answers"> ${answers.join('')} </div>`
                     );
                 }
+            );
 
-                // add this question and its answers to the output
-                output.push(
-                    `<div class="question"> ${currentQuestion.question} </div>
-                    <div class="answers"> ${answers.join('')} </div>`
-                );
-            }
-        );
+            // finally combine our output list into one string of HTML and put it on the page
+            quizContainer.html(output.join(''));
+        }
 
-        // finally combine our output list into one string of HTML and put it on the page
-        quizContainer.innerHTML = output.join('');
-    }
+        function showResults() {
+            // gather answer containers from our quiz
+            const answerContainers = quizContainer.find('.answers');
 
-    function showResults() {
-        // gather answer containers from our quiz
-        const answerContainers = quizContainer.querySelectorAll('.answers');
+            // keep track of user's answers
+            let numCorrect = 0;
 
-        // keep track of user's answers
-        let numCorrect = 0;
+            // for each question...
+            myQuestions.forEach((currentQuestion, questionNumber) => {
 
-        // for each question...
-        myQuestions.forEach((currentQuestion, questionNumber) => {
+                // find selected answer
+                const answerContainer = answerContainers[questionNumber];
+                const selector = 'input[name=question' + questionNumber + ']:checked';
+                const userAnswer = ($(answerContainer).find(selector) || {}).val();
 
-            // find selected answer
-            const answerContainer = answerContainers[questionNumber];
-            const selector = 'input[name=question' + questionNumber + ']:checked';
-            const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+                // if answer is correct
+                if (userAnswer === currentQuestion.correctAnswer) {
+                    // add to the number of correct answers
+                    numCorrect++;
 
-            // if answer is correct
-            if (userAnswer === currentQuestion.correctAnswer) {
-                // add to the number of correct answers
-                numCorrect++;
+                    // color the answers green
+                    $(answerContainers[questionNumber]).css("color", "lightgreen");
+                }
+                // if answer is wrong or blank
+                else {
+                    // color the answers red
+                    $(answerContainers[questionNumber]).css("color", "red");
+                }
+            });
 
-                // color the answers green
-                answerContainers[questionNumber].style.color = 'lightgreen';
-            }
-            // if answer is wrong or blank
-            else {
-                // color the answers red
-                answerContainers[questionNumber].style.color = 'red';
-            }
-        });
+            // show number of correct answers out of total
+            resultsContainer.html(numCorrect + ' out of ' + myQuestions.length);
+        }
 
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
-    }
+        // display quiz right away
+        buildQuiz();
 
-    // display quiz right away
-    buildQuiz();
-
-    // on submit, show result
-    submitButton.click(showResults);
-
-    const myQuestions = [{
-        question: "Who is the strongest?",
-        answers: {
-            a: "Superman",
-            b: "The Terminator",
-            c: "Waluigi, obviously"
-        },
-        correctAnswer: "c"
-    }, {
-        question: "What is the best site ever created?",
-        answers: {
-            a: "SitePoint",
-            b: "Simple Steps Code",
-            c: "Trick question; they're both the best"
-        },
-        correctAnswer: "c"
-    }, {
-        question: "Where is Waldo really?",
-        answers: {
-            a: "Antarctica",
-            b: "Exploring the Pacific Ocean",
-            c: "Sitting in a tree",
-            d: "Minding his own business, so stop asking"
-        },
-        correctAnswer: "d"
-    }];
-
+        // on submit, show result
+        submitButton.click(showResults);
+    })();
 
 });
