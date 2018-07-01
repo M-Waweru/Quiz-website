a23c5249a5ab1a8ca40c1b56b79c89b35698456f
johnngugi <johnngugi777@gmail.com>
2018-07-01 21:19:48 +0300

    Change mode

--- php/connection.php
+++ php/connection.php
@@ -1,6 +1,5 @@
 <?php
-
-$mysqli = new mysqli("localhost", "root", "", "quiz-website");
+require 'my_custom_connection.php';
 
 if($mysqli === false){
 	die("WARNING!!!! Could not connect. " . $mysqli->connect_error);
