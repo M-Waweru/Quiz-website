6d10404505b0bf4baf20aa86174d77465fdf0ca9
M-Waweru <mathenge.waweru@stratmore.edu>
2018-07-02 12:28:25 +0300

    Debugging login ongoing

--- php/loginauthenticate.php
+++ php/loginauthenticate.php
@@ -12,21 +12,21 @@ $result = $mysqli->query($qry);
 
 if ($result===true || mysqli_num_rows($result) > 0){
 	while ($row=$result->fetch_assoc()){
-		$hashed=password_hash($pwd, PASSWORD_DEFAULT);
-		$hash=password_verify($pwd, $row["Password"]);
-		echo $row["Password"]."<br>";
-		echo $hashed;
-		if ($hash==false){
-			$_SESSION["passwordcheck"]=false;
-			echo "Wrong password";
-			//header('Location: ../loginpage.php');
-		} else {
+		//$hashed=password_hash($pwd, PASSWORD_DEFAULT);
+		//$hash=;
+		//echo $row["Password"]."<br>";
+		//echo $hashed;
+		if (password_verify($pwd, $row["Password"])){
 			$_SESSION["passwordcheck"]=true;
 			$_SESSION["username"]=$username;
 			$_SESSION["usernamecheck"]=true;
 
 			echo "You are logged in";
-			header('Location: ../index.php');		
+			header('Location: ../index.php');
+		} else {
+			$_SESSION["passwordcheck"]=false;
+			echo "Wrong password";
+			//header('Location: ../loginpage.php');
 		}
 	}
 } else {
